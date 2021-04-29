75c6d58c5c7a2caa9dd0f5a5dd35736c9cba35cd
RASEL KIBRIA <rasel5jkibria@gmail.com>
2021-04-29 21:12:36 +0600

    DataAccess Layer added, Employees table added

---
+++ Computer Shop Inventory Management/Data Access Layer/DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Shop_Inventory_Management.Data_Access_Layer
+{
+        class DataAccess : IDisposable
+        {
+            protected SqlConnection connection;
+            protected SqlCommand command;
+
+            public DataAccess()
+            {
+                try
+                {
+                    this.connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ComputerInventoryManagement"].ConnectionString);
+                    this.connection.Open();
+                }
+                catch (Exception exp)
+                {
+
+                }
+            }
+
+            public SqlDataReader GetData(string sql)
+            {
+                this.command = new SqlCommand(sql, connection);
+                return this.command.ExecuteReader();
+            }
+            public int ExecuteQuery(string sql)
+            {
+                try
+                {
+                    this.command = new SqlCommand(sql, connection);
+
+                }
+                catch (Exception e)
+                {
+                    //MessageBox.Show(e.Message);
+                }
+                return this.command.ExecuteNonQuery();
+            }
+
+            public void Dispose()
+            {
+                this.connection.Close();
+            }
+
+            ~DataAccess()
+            {
+                //this.connection.Close();
+            }
+        }
+}
