f5b0be1994305c843f6f35736742b84207a3234e
Refat1998 <mdrefa444@gmail.com>
2021-05-01 19:48:42 +0600

    Admin Home Updated

--- Computer Shop Inventory Management/Presentation Layer/AdminHome.Designer.cs
+++ Computer Shop Inventory Management/Presentation Layer/AdminHome.Designer.cs
@@ -34,16 +34,17 @@ namespace Computer_Shop_Inventory_Management
             this.modifyButton = new System.Windows.Forms.Button();
             this.sellButton = new System.Windows.Forms.Button();
             this.adminGroupBox = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalSaleLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.yearlyRadioButton = new System.Windows.Forms.RadioButton();
+            this.monthlyRadioButton = new System.Windows.Forms.RadioButton();
+            this.dailyRadioButton = new System.Windows.Forms.RadioButton();
             this.logoutButton = new System.Windows.Forms.Button();
             this.viewProdcutButton = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dailyRadioButton = new System.Windows.Forms.RadioButton();
-            this.monthlyRadioButton = new System.Windows.Forms.RadioButton();
-            this.yearlyRadioButton = new System.Windows.Forms.RadioButton();
-            this.totalSaleLabel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.selectButton = new System.Windows.Forms.Button();
+            this.saleInfoButton = new System.Windows.Forms.Button();
             this.adminGroupBox.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -51,9 +52,10 @@ namespace Computer_Shop_Inventory_Management
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Location = new System.Drawing.Point(8, 23);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 30);
+            this.button1.Size = new System.Drawing.Size(172, 37);
             this.button1.TabIndex = 1;
             this.button1.Text = "Add Employee";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,9 +64,10 @@ namespace Computer_Shop_Inventory_Management
             // addProductButton
             // 
             this.addProductButton.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addProductButton.Location = new System.Drawing.Point(6, 116);
+            this.addProductButton.Location = new System.Drawing.Point(8, 143);
+            this.addProductButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.addProductButton.Name = "addProductButton";
-            this.addProductButton.Size = new System.Drawing.Size(129, 30);
+            this.addProductButton.Size = new System.Drawing.Size(172, 37);
             this.addProductButton.TabIndex = 2;
             this.addProductButton.Text = "Add Product";
             this.addProductButton.UseVisualStyleBackColor = true;
@@ -73,9 +76,10 @@ namespace Computer_Shop_Inventory_Management
             // modifyButton
             // 
             this.modifyButton.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.modifyButton.Location = new System.Drawing.Point(6, 67);
+            this.modifyButton.Location = new System.Drawing.Point(8, 82);
+            this.modifyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.modifyButton.Name = "modifyButton";
-            this.modifyButton.Size = new System.Drawing.Size(129, 30);
+            this.modifyButton.Size = new System.Drawing.Size(172, 37);
             this.modifyButton.TabIndex = 4;
             this.modifyButton.Text = "Update Employee";
             this.modifyButton.UseVisualStyleBackColor = true;
@@ -84,15 +88,17 @@ namespace Computer_Shop_Inventory_Management
             // sellButton
             // 
             this.sellButton.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sellButton.Location = new System.Drawing.Point(177, 19);
+            this.sellButton.Location = new System.Drawing.Point(236, 23);
+            this.sellButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sellButton.Name = "sellButton";
-            this.sellButton.Size = new System.Drawing.Size(145, 30);
+            this.sellButton.Size = new System.Drawing.Size(193, 37);
             this.sellButton.TabIndex = 5;
             this.sellButton.Text = "Total Sale";
             this.sellButton.UseVisualStyleBackColor = true;
             // 
             // adminGroupBox
             // 
+            this.adminGroupBox.Controls.Add(this.saleInfoButton);
             this.adminGroupBox.Controls.Add(this.label3);
             this.adminGroupBox.Controls.Add(this.label2);
             this.adminGroupBox.Controls.Add(this.totalSaleLabel);
@@ -103,20 +109,114 @@ namespace Computer_Shop_Inventory_Management
             this.adminGroupBox.Controls.Add(this.viewProdcutButton);
             this.adminGroupBox.Controls.Add(this.addProductButton);
             this.adminGroupBox.Controls.Add(this.modifyButton);
-            this.adminGroupBox.Location = new System.Drawing.Point(20, 12);
+            this.adminGroupBox.Location = new System.Drawing.Point(27, 15);
+            this.adminGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.adminGroupBox.Name = "adminGroupBox";
-            this.adminGroupBox.Size = new System.Drawing.Size(768, 426);
+            this.adminGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.adminGroupBox.Size = new System.Drawing.Size(1024, 524);
             this.adminGroupBox.TabIndex = 6;
             this.adminGroupBox.TabStop = false;
             this.adminGroupBox.Text = "Admin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(579, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(513, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 10;
+            // 
+            // totalSaleLabel
+            // 
+            this.totalSaleLabel.AutoSize = true;
+            this.totalSaleLabel.Location = new System.Drawing.Point(480, 82);
+            this.totalSaleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.totalSaleLabel.Name = "totalSaleLabel";
+            this.totalSaleLabel.Size = new System.Drawing.Size(72, 17);
+            this.totalSaleLabel.TabIndex = 9;
+            this.totalSaleLabel.Text = "Total Sale";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.selectButton);
+            this.groupBox1.Controls.Add(this.yearlyRadioButton);
+            this.groupBox1.Controls.Add(this.monthlyRadioButton);
+            this.groupBox1.Controls.Add(this.dailyRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(236, 82);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(193, 198);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(48, 155);
+            this.selectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(95, 27);
+            this.selectButton.TabIndex = 12;
+            this.selectButton.Text = "Select";
+            this.selectButton.UseVisualStyleBackColor = true;
+            // 
+            // yearlyRadioButton
+            // 
+            this.yearlyRadioButton.AutoSize = true;
+            this.yearlyRadioButton.Location = new System.Drawing.Point(60, 105);
+            this.yearlyRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.yearlyRadioButton.Name = "yearlyRadioButton";
+            this.yearlyRadioButton.Size = new System.Drawing.Size(69, 21);
+            this.yearlyRadioButton.TabIndex = 2;
+            this.yearlyRadioButton.TabStop = true;
+            this.yearlyRadioButton.Text = "Yearly";
+            this.yearlyRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // monthlyRadioButton
+            // 
+            this.monthlyRadioButton.AutoSize = true;
+            this.monthlyRadioButton.Location = new System.Drawing.Point(60, 76);
+            this.monthlyRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.monthlyRadioButton.Name = "monthlyRadioButton";
+            this.monthlyRadioButton.Size = new System.Drawing.Size(78, 21);
+            this.monthlyRadioButton.TabIndex = 1;
+            this.monthlyRadioButton.TabStop = true;
+            this.monthlyRadioButton.Text = "Monthly";
+            this.monthlyRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // dailyRadioButton
+            // 
+            this.dailyRadioButton.AutoSize = true;
+            this.dailyRadioButton.Location = new System.Drawing.Point(60, 44);
+            this.dailyRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dailyRadioButton.Name = "dailyRadioButton";
+            this.dailyRadioButton.Size = new System.Drawing.Size(60, 21);
+            this.dailyRadioButton.TabIndex = 0;
+            this.dailyRadioButton.TabStop = true;
+            this.dailyRadioButton.Text = "Daily";
+            this.dailyRadioButton.UseVisualStyleBackColor = true;
             // 
             // logoutButton
             // 
             this.logoutButton.BackColor = System.Drawing.SystemColors.Control;
             this.logoutButton.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logoutButton.Location = new System.Drawing.Point(6, 391);
+            this.logoutButton.Location = new System.Drawing.Point(8, 481);
+            this.logoutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.logoutButton.Name = "logoutButton";
-            this.logoutButton.Size = new System.Drawing.Size(84, 29);
+            this.logoutButton.Size = new System.Drawing.Size(112, 36);
             this.logoutButton.TabIndex = 7;
             this.logoutButton.Text = "Log Out";
             this.logoutButton.UseVisualStyleBackColor = false;
@@ -125,102 +225,31 @@ namespace Computer_Shop_Inventory_Management
             // viewProdcutButton
             // 
             this.viewProdcutButton.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.viewProdcutButton.Location = new System.Drawing.Point(6, 163);
+            this.viewProdcutButton.Location = new System.Drawing.Point(8, 201);
+            this.viewProdcutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.viewProdcutButton.Name = "viewProdcutButton";
-            this.viewProdcutButton.Size = new System.Drawing.Size(129, 30);
+            this.viewProdcutButton.Size = new System.Drawing.Size(172, 37);
             this.viewProdcutButton.TabIndex = 2;
             this.viewProdcutButton.Text = "View Product";
             this.viewProdcutButton.UseVisualStyleBackColor = true;
             this.viewProdcutButton.Click += new System.EventHandler(this.viewProdcutButton_Click);
             // 
-            // groupBox1
+            // saleInfoButton
             // 
-            this.groupBox1.Controls.Add(this.selectButton);
-            this.groupBox1.Controls.Add(this.yearlyRadioButton);
-            this.groupBox1.Controls.Add(this.monthlyRadioButton);
-            this.groupBox1.Controls.Add(this.dailyRadioButton);
-            this.groupBox1.Location = new System.Drawing.Point(177, 67);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(145, 161);
-            this.groupBox1.TabIndex = 8;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // dailyRadioButton
-            // 
-            this.dailyRadioButton.AutoSize = true;
-            this.dailyRadioButton.Location = new System.Drawing.Point(45, 36);
-            this.dailyRadioButton.Name = "dailyRadioButton";
-            this.dailyRadioButton.Size = new System.Drawing.Size(48, 17);
-            this.dailyRadioButton.TabIndex = 0;
-            this.dailyRadioButton.TabStop = true;
-            this.dailyRadioButton.Text = "Daily";
-            this.dailyRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // monthlyRadioButton
-            // 
-            this.monthlyRadioButton.AutoSize = true;
-            this.monthlyRadioButton.Location = new System.Drawing.Point(45, 62);
-            this.monthlyRadioButton.Name = "monthlyRadioButton";
-            this.monthlyRadioButton.Size = new System.Drawing.Size(62, 17);
-            this.monthlyRadioButton.TabIndex = 1;
-            this.monthlyRadioButton.TabStop = true;
-            this.monthlyRadioButton.Text = "Monthly";
-            this.monthlyRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // yearlyRadioButton
-            // 
-            this.yearlyRadioButton.AutoSize = true;
-            this.yearlyRadioButton.Location = new System.Drawing.Point(45, 85);
-            this.yearlyRadioButton.Name = "yearlyRadioButton";
-            this.yearlyRadioButton.Size = new System.Drawing.Size(54, 17);
-            this.yearlyRadioButton.TabIndex = 2;
-            this.yearlyRadioButton.TabStop = true;
-            this.yearlyRadioButton.Text = "Yearly";
-            this.yearlyRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // totalSaleLabel
-            // 
-            this.totalSaleLabel.AutoSize = true;
-            this.totalSaleLabel.Location = new System.Drawing.Point(360, 67);
-            this.totalSaleLabel.Name = "totalSaleLabel";
-            this.totalSaleLabel.Size = new System.Drawing.Size(55, 13);
-            this.totalSaleLabel.TabIndex = 9;
-            this.totalSaleLabel.Text = "Total Sale";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(385, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
-            this.label2.TabIndex = 10;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(434, 67);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "label3";
-            this.label3.Visible = false;
-            // 
-            // selectButton
-            // 
-            this.selectButton.Location = new System.Drawing.Point(36, 126);
-            this.selectButton.Name = "selectButton";
-            this.selectButton.Size = new System.Drawing.Size(71, 22);
-            this.selectButton.TabIndex = 12;
-            this.selectButton.Text = "Select";
-            this.selectButton.UseVisualStyleBackColor = true;
+            this.saleInfoButton.Location = new System.Drawing.Point(8, 258);
+            this.saleInfoButton.Name = "saleInfoButton";
+            this.saleInfoButton.Size = new System.Drawing.Size(172, 37);
+            this.saleInfoButton.TabIndex = 12;
+            this.saleInfoButton.Text = "Sale Details";
+            this.saleInfoButton.UseVisualStyleBackColor = true;
             // 
             // AdminHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.adminGroupBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdminHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminHome";
@@ -249,5 +278,6 @@ namespace Computer_Shop_Inventory_Management
         private System.Windows.Forms.RadioButton yearlyRadioButton;
         private System.Windows.Forms.RadioButton monthlyRadioButton;
         private System.Windows.Forms.RadioButton dailyRadioButton;
+        private System.Windows.Forms.Button saleInfoButton;
     }
 }
